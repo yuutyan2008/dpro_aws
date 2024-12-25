655b496a59dfcf1a9afe944f703a9cbf3a5222a4
takanosatomi <ricebread778899@gmail.com>
2024-12-25 18:05:19 +0900

    seed削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,19 +17,19 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/uploads}
 after 'deploy:published', 'deploy:seed'   # 9
 after 'deploy:finished', 'deploy:restart'   # 10
 
-namespace :deploy do
-  desc 'Run seed'
-  task :seed do
-    on roles(:db) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
-        end
-      end
-    end
-  end
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
+# namespace :deploy do
+#   desc 'Run seed'
+#   task :seed do
+#     on roles(:db) do
+#       with rails_env: fetch(:rails_env) do
+#         within current_path do
+#           execute :bundle, :exec, :rake, 'db:seed'
+#         end
+#       end
+#     end
+#   end
+#   desc 'Restart application'
+#   task :restart do
+#     invoke 'unicorn:restart'
+#   end
+# end
