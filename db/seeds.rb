1bc41273f13e691c427b613e2a12e143f79d529f
takanosatomi <ricebread778899@gmail.com>
2024-12-25 18:01:58 +0900

    unicone

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  num = n + 1
+  Task.find_or_create_by!(id: num) do |task|
+    task.title = "title#{num}"
+    task.content = "content#{num}"
+  end
+end
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
